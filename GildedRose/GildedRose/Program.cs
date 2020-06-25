16a288a1d0603d7a9d67f563d6a72e31bd68b681
Paul Gradie <paul.gradie@octopus.com>
2020-06-25 18:10:26 +1000

    simplified to switch

--- GildedRose/GildedRose/Program.cs
+++ GildedRose/GildedRose/Program.cs
@@ -13,22 +13,22 @@ namespace GildedRose
             Console.WriteLine("OMGHAI!");
 
             var app = new Program()
-                          {
-                              Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
-                          };
+            {
+                Items = new List<Item>
+                {
+                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                    new Item
+                    {
+                        Name = "Backstage passes to a TAFKAL80ETC concert",
+                        SellIn = 15,
+                        Quality = 20
+                    },
+                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                }
+            };
 
             static void showItems(IEnumerable<Item> items)
             {
@@ -37,100 +37,108 @@ namespace GildedRose
                     Console.WriteLine(item.ToString());
                 }
             }
-            
-            Console.WriteLine("BEFORE UPDATE");
-            showItems(app.Items);
-            app.UpdateQuality();
-            Console.WriteLine("AFTER UPDATE");
-            showItems(app.Items);
-            Console.ReadKey();
 
+            showItems(app.Items);
+
+        }
+
+        static void decrementQualityBy(Item item, int value = 1)
+        {
+            if (item.Quality >= (0 + value))
+            {
+                item.Quality -= (item.SellIn == 0) ? value * 2 : value;
+            }
+        }
+
+        static void incrementQualityBy(Item item, int value = 1)
+        {
+            if (item.Quality <= (50 - value))
+            {
+                item.Quality += value;
+            }
+        }
+
+        void DecrementSellInBy(Item item, int value = 1)
+        {
+            if (item.SellIn > (0 + value))
+            {
+                item.SellIn -= value;
+            }
+        }
+
+        void updateSulfuras()
+        {
+        }
+
+        void UpdateDexterityVest(Item item)
+        {
+            decrementQualityBy(item, 1);
+            DecrementSellInBy(item, 1);
+        }
+
+        void UpdateManaCake(Item item)
+        {
+            decrementQualityBy(item, 2);
+            DecrementSellInBy(item, 1);
+        }
+
+        void UpdateBackstagePass(Item item)
+        {
+            int value;
+            if (item.SellIn < 6)
+            {
+                value = 3;
+            }
+            else if (item.SellIn < 11)
+            {
+                value = 2;
+            }
+            else
+            {
+                value = 1;
+            }
+
+            incrementQualityBy(item, value);
+            DecrementSellInBy(item, 1);
+            item.Quality = item.SellIn == 0 ? 0 : item.Quality;
+        }
+
+        void UpdateElixir(Item item)
+        {
+            decrementQualityBy(item, 1);
+            DecrementSellInBy(item, 1);
+        }
+
+        void UpdateBrie(Item item)
+        {
+            incrementQualityBy(item, 1);
+            DecrementSellInBy(item, 1);
         }
 
         public void UpdateQuality()
         {
-            foreach (var t in Items)
+            foreach (var item in Items)
             {
-                if (t.Name != "Aged Brie" && t.Name != "Backstage passes to a TAFKAL80ETC concert")
+                switch (item.Name)
                 {
-                    if (t.Quality >= 0)
-                    {
-                        if (t.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            if (t.Quality > 0)
-                            {
-                                if (t.Name != "Conjured Mana Cake")
-                                {
-                                    t.Quality -= 1;
-                                }
-                                else
-                                {
-                                    t.Quality = ( t.Quality > 2 ) ? (t.Quality - 2) : 0;
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (t.Quality < 50)
-                    {
-                        t.Quality += 1;
-
-                        if (t.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (t.SellIn < 11)
-                            {
-                                if (t.Quality < 50)
-                                {
-                                    t.Quality += 1;
-                                }
-                            }
-
-                            if (t.SellIn < 6)
-                            {
-                                if (t.Quality < 50)
-                                {
-                                    t.Quality += 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (t.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    if (t.SellIn > 0)
-                    {
-                        t.SellIn -= 1;
-                    }
-                }
-
-                if (t.SellIn > 0) continue;
-                if (t.Name != "Aged Brie")
-                {
-                    if (t.Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (t.Quality <= 0) continue;
-                        if (t.Name == "Sulfuras, Hand of Ragnaros") continue;
-                        if (t.Quality > 0)
-                        {
-                            t.Quality -= 1;
-                        }
-                    }
-                    else
-                    {
-                        t.Quality = 0;
-
-                    }
-                }
-                else
-                {
-                    if (t.Quality >= 50) continue;
-                    if (t.Quality >= 0)
-                    {
-                        t.Quality += 1;
-                    }
+                    case ("Sulfuras, Hand of Ragnaros"):
+                        updateSulfuras();
+                        break;
+                    case ("Backstage passes to a TAFKAL80ETC concert"):
+                        UpdateBackstagePass(item);
+                        break;
+                    case ("+5 Dexterity Vest"):
+                        UpdateDexterityVest(item);
+                        break;
+                    case ("Elixir of the Mongoose"):
+                        UpdateElixir(item);
+                        break;
+                    case ("Aged Brie"):
+                        UpdateBrie(item);
+                        break;
+                    case ("Conjured Mana Cake"):
+                        UpdateManaCake(item);
+                        break;
                 }
             }
         }
